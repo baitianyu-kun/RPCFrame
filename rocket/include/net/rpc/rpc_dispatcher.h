b05277c2221cbeab7251f18a966529e4cb4b8694
baitianyu <1748383809@qqq.com>
2024-07-25 01:30:11 -0700

    rpc dispatcher

---
+++ rocket/include/net/rpc/rpc_dispatcher.h
@@ -0,0 +1,39 @@
+//
+// Created by baitianyu on 7/24/24.
+//
+
+#ifndef RPCFRAME_RPC_DISPATCHER_H
+#define RPCFRAME_RPC_DISPATCHER_H
+
+#include <map>
+#include <memory>
+#include "net/coder/abstract_protocol.h"
+#include "net/coder/tinypb_protocol.h"
+#include "google/protobuf/service.h"
+#include "google/protobuf/descriptor.h"
+#include "google/protobuf/message.h"
+
+namespace rocket {
+
+    class RPCDispatcher {
+    public:
+        using protobuf_service_sptr_t_ = std::shared_ptr<google::protobuf::Service>;
+
+        void dispatch(const AbstractProtocol::abstract_pro_sptr_t_ &request,
+                      const AbstractProtocol::abstract_pro_sptr_t_ &response);
+
+        void registerService(protobuf_service_sptr_t_ service);
+
+        void setTinyPBError(std::shared_ptr<TinyPBProtocol> &msg, int32_t err_code, const std::string &err_info);
+
+    private:
+        bool parseServiceFullName(const std::string &full_name, std::string &service_name, std::string &method_name);
+
+    private:
+        std::map<std::string, protobuf_service_sptr_t_> m_service_map;
+    };
+
+}
+
+
+#endif //RPCFRAME_RPC_DISPATCHER_H
---
+++ rocket/src/net/rpc/rpc_dispatcher.cpp
@@ -0,0 +1,93 @@
+//
+// Created by baitianyu on 7/24/24.
+//
+#include "net/rpc/rpc_dispatcher.h"
+#include "common/log.h"
+#include "common/error_code.h"
+
+namespace rocket {
+
+    void rocket::RPCDispatcher::dispatch(const AbstractProtocol::abstract_pro_sptr_t_ &request,
+                                         const AbstractProtocol::abstract_pro_sptr_t_ &response) {
+        auto req_protocol = std::dynamic_pointer_cast<TinyPBProtocol>(request);
+        auto rsp_protocol = std::dynamic_pointer_cast<TinyPBProtocol>(response);
+        // rpc调用method为：Order.makeOrder，前面是service名，后面是方法名
+        auto method_full_name = req_protocol->m_method_name;
+        std::string service_name;
+        std::string method_name;
+        // 赋予相同的id，二者的方法名肯定也一样
+        rsp_protocol->m_msg_id = req_protocol->m_msg_id;
+        rsp_protocol->m_method_name = req_protocol->m_method_name;
+        if (!parseServiceFullName(method_full_name, service_name, method_name)) {
+            setTinyPBError(rsp_protocol, ERROR_PARSE_SERVICE_NAME, "parse service name error");
+            return;
+        }
+
+        auto iter = m_service_map.find(service_name);
+        if (iter == m_service_map.end()) {
+            ERRORLOG("%s | service name [%s] not found", req_protocol->m_msg_id.c_str(), service_name.c_str());
+            setTinyPBError(rsp_protocol, ERROR_SERVICE_NOT_FOUND, "service not found");
+            return;
+        }
+
+        auto service = (*iter).second;
+        auto method = service->GetDescriptor()->FindMethodByName(method_name);
+        if (method == nullptr) {
+            ERRORLOG("%s | method name [%s] not found in service[%s]", req_protocol->m_msg_id.c_str(),
+                     method_name.c_str(), service_name.c_str());
+            setTinyPBError(rsp_protocol, ERROR_SERVICE_NOT_FOUND, "method not found");
+            return;
+        }
+
+        auto req_msg = std::shared_ptr<google::protobuf::Message>(service->GetRequestPrototype(method).New());
+        // 反序列化，将 pb_data 反序列化为 req_msg
+        // ParseFromString传的是引用
+        if (!req_msg->ParseFromString(req_protocol->m_pb_data)) {
+            ERRORLOG("%s | deserialize error", req_protocol->m_msg_id.c_str(), method_name.c_str(),
+                     service_name.c_str());
+            setTinyPBError(rsp_protocol, ERROR_FAILED_DESERIALIZE, "deserialize error");
+            return;
+        }
+        INFOLOG("%s | get rpc request[%s]", req_protocol->m_msg_id.c_str(), req_msg->ShortDebugString().c_str());
+
+        auto rsp_msg = std::shared_ptr<google::protobuf::Message>(service->GetResponsePrototype(method).New());
+
+
+    }
+
+    void RPCDispatcher::registerService(RPCDispatcher::protobuf_service_sptr_t_ service) {
+        auto service_name = service->GetDescriptor()->full_name();
+        m_service_map[service_name] = service;
+    }
+
+    bool RPCDispatcher::parseServiceFullName(const std::string &full_name, std::string &service_name,
+                                             std::string &method_name) {
+        if (full_name.empty()) {
+            ERRORLOG("full name empty");
+            return false;
+        }
+        // Order.makeOrder
+        auto i = full_name.find_first_of(".");
+        // find函数在找不到指定值得情况下会返回string::npos
+        if (i == full_name.npos) {
+            ERRORLOG("not find '.' in full name [%s]", full_name.c_str());
+            return false;
+        }
+        service_name = full_name.substr(0, i);
+        method_name = full_name.substr(i + 1, full_name.length() - i - 1);
+        INFOLOG("parse service_name [%s] and method_name [%s] from full name [%s]", service_name.c_str(),
+                method_name.c_str(), full_name.c_str());
+        return true;
+    }
+
+    void RPCDispatcher::setTinyPBError(std::shared_ptr<TinyPBProtocol> &msg, int32_t err_code,
+                                       const std::string &err_info) {
+        msg->m_err_code = err_code;
+        msg->m_err_info = err_info;
+        msg->m_err_info_len = err_info.length();
+    }
+
+}
+
+
+

90dbb87d711a4009ca7b9657593ed0bda89af73d
Cris@None <63059666+baitianyu-kun@users.noreply.github.com>
2024-10-29 23:31:31 +0800

    Update hash_balance.cpp

--- rocket/src/net/balance/hash_balance.cpp
+++ rocket/src/net/balance/hash_balance.cpp
@@ -73,6 +73,32 @@ namespace rocket {
         }
         return all_server_node.substr(0, all_server_node.length() - 1);
     }
+
+// 测试负载量
+//    auto hash = make_shared<ConsistentHash>(50);
+//    hash->AddNewPhysicalNode("192.168.1.1:8080");
+//    hash->AddNewPhysicalNode("192.168.1.2:8080");
+//    hash->AddNewPhysicalNode("192.168.1.3:8080");
+//    hash->AddNewPhysicalNode("192.168.1.4:8080");
+//    unordered_map<string, int> maps; // 记录每个地址的访问量
+//
+//    srand((unsigned) time(NULL));
+//    int a = 0, b = 192;
+//    int fangwen_cishu = 5000000;
+//    for (int i = 0; i < fangwen_cishu; ++i) {
+//        auto rand1 = (rand() % (b - a + 1)) + a;
+//        auto rand2 = (rand() % (b - a + 1)) + a;
+//        auto rand3 = (rand() % (b - a + 1)) + a;
+//        auto rand4 = (rand() % (b - a + 1)) + a;
+//        string addr =
+//                to_string(rand1) + "." + to_string(rand2) + "." + to_string(rand3) + "." + to_string(rand4) + ":80";
+//        maps[hash->GetServerIndex(addr)]++;
+//    }
+//
+//    for (const auto &item: maps) {
+//        double rate = ((1.0 * item.second) / fangwen_cishu) * 100;
+//        cout << "Node: " << item.first << "        Rate:" << rate << endl;
+//    }
 }
 
 
